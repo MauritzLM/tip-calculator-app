f468214a6061c74b1a5c6d94c22e9f9a98749233
Mauritz Marais <mauritz303@gmail.com>
2023-01-08 15:12:14 +0200

    added eventlisteners to run calculate function on input and to display percentage selected style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { calculator } from "./calculator";
-import { displayAmount } from "./dom";
+import { displayAmount, getTipPercentage } from "./dom";
 
 const myCalculator = calculator();
 
@@ -11,3 +11,47 @@ const percentageSelect = document.querySelectorAll(".select-tip-section div");
 const customPercentage = document.querySelector("#custom-percentage");
 // number of people input
 const numberOfPeople = document.querySelector("#people");
+
+// display totals
+const displayTip = document.querySelector(".display-tip-amount");
+const displayTotal = document.querySelector(".display-total-amount");
+
+// event listener to calculate amount when number inputs are used
+document.addEventListener("input", (e) => {
+    if (e.target.matches("input[type='number']")) {
+        // calulate tip pp 
+        let tip = getTipPercentage(percentageSelect, customPercentage.value, "selected");
+
+        let tipAmount = myCalculator.calculateTip(billTotal.value, tip);
+        let calculatedTip = myCalculator.tipPerPerson(tipAmount, numberOfPeople.value);
+
+        // calculate total pp
+        let calculatedTotal = myCalculator.totalPerPerson(Number(billTotal.value), tipAmount, numberOfPeople.value);
+
+        // update display
+        displayAmount(displayTip, calculatedTip);
+        displayAmount(displayTotal, calculatedTotal);
+    }
+});
+
+// select percentage add class on click
+document.addEventListener("click", (e) => {
+    if (e.target.matches(".select-tip-section div")) {
+        percentageSelect.forEach(div => {
+            div.className = "";
+        });
+        customPercentage.value = '';
+        e.target.className = "selected";
+
+        // calculate totals*
+    };
+
+    if (e.target.matches("#custom-percentage")) {
+        percentageSelect.forEach(div => {
+            div.className = "";
+        });
+    };
+});
+
+// reset button*
+
