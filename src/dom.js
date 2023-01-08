96c99bac7092d93748148b59b088bc5162b56f6f
Mauritz Marais <mauritz303@gmail.com>
2023-01-08 15:07:36 +0200

    added function to get percentage from document

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,22 @@
 
 // display amount function
 export const displayAmount = (element, amount) => {
-    element.textContent = `${amount}`;
+    element.textContent = `$ ${amount.toLocaleString("en-GB", { minimumFractionDigits: 2 })}`;
 }
 
+export const getTipPercentage = (arr, value, style) => {
+    let percentage;
+    if (!value) {
+        arr.forEach(element => {
+            if (element.classList.contains(style)) {
+                // get only the number
+                percentage = element.textContent.slice(0, -1);
+            }
 
+        });
+
+        return percentage;
+    }
+
+    return value;
+}
